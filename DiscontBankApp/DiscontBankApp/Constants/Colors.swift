14915b68e140d79dc58c88eb8f4f25341c0f959c
Niv Ben-porath <nivbp7@users.noreply.github.com>
2022-05-04 10:53:30 +0300

    Update color and build to 34

--- DiscontBankApp/DiscontBankApp/Constants/Colors.swift
+++ DiscontBankApp/DiscontBankApp/Constants/Colors.swift
@@ -15,5 +15,5 @@ extension UIColor {
     static let highColor = UIColor.systemGreen
     static let lowColor = UIColor.systemRed
     static let closeColor = UIColor.systemPurple
-    static let volumeColor = UIColor.systemPink
+    static let volumeColor = UIColor.systemMint
 }
